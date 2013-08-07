37f057c963f520642f4eaed6ba486bf7cd54770c
tuxman666 <tuxman666@users.sourceforge.net>
2013-08-07 11:14:29 +0000

    Wunschumsetzung :)

--- Tools/AStyle.bat
+++ Tools/AStyle.bat
@@ -1,4 +1,4 @@
 @echo off
-.\astyle\bin\astyle --style=allman --pad-header --unpad-paren --recursive --exclude=Tools ..\*.cpp ..\*.h
+.\astyle\bin\astyle --style=allman --keep-one-line-blocks --keep-one-line-statements --indent-switches --pad-header --unpad-paren --recursive --exclude=Tools ..\*.cpp ..\*.h
 cd ..
 del /S *.orig
