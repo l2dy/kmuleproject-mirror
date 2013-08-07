09f9ed64cdfd0f52eb6064f5340a8a2232cbf14a
tuxman666 <tuxman666@users.sourceforge.net>
2013-08-07 11:47:00 +0000

    "kein Backup" geht auch einfacher

--- Tools/AStyle.bat
+++ Tools/AStyle.bat
@@ -1,4 +1,2 @@
 @echo off
-.\astyle\bin\astyle --style=allman --keep-one-line-blocks --keep-one-line-statements --indent-switches --pad-header --unpad-paren --recursive --exclude=Tools ..\*.cpp ..\*.h
-cd ..
-del /S *.orig
+.\astyle\bin\astyle --style=allman --keep-one-line-blocks --keep-one-line-statements --indent-switches --pad-header --unpad-paren --recursive --suffix=none --exclude=Tools ..\*.cpp ..\*.h
