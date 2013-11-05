3463148414112c7e7b32603463d9ba647c29e0e4
tuxman666 <tuxman666@users.sourceforge.net>
2013-11-05 23:58:01 +0000

    astyle 2.04

--- Tools/AStyle.bat
+++ Tools/AStyle.bat
@@ -1,2 +1,2 @@
 @echo off
-.\astyle\bin\astyle --style=allman --keep-one-line-blocks --keep-one-line-statements --indent-switches --pad-header --unpad-paren --recursive --suffix=none --exclude=Tools ..\*.cpp ..\*.h
+.\astyle\bin\astyle --style=allman --keep-one-line-blocks --keep-one-line-statements --indent-modifiers --indent-switches --pad-header --unpad-paren --recursive --suffix=none --exclude=Tools ..\*.cpp ..\*.h
